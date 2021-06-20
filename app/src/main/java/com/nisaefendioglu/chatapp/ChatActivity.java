2f9b5e018e504605b6d5e92a8b857856fce451f4
nisaefendioglu <nisaefendioglu0@gmail.com>
2021-06-20 14:02:17 +0300

    theme edited.

--- app/src/main/java/com/nisaefendioglu/chatapp/ChatActivity.java
+++ app/src/main/java/com/nisaefendioglu/chatapp/ChatActivity.java
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -49,6 +52,7 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
 
 
         userName = getIntent().getExtras().getString("username");
